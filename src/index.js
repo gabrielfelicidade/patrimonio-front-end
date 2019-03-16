c4551cb55638ce7ebaaf1ae7d4e566672102ad94
sernajoto <guilhermesernajoto@hotmail.com>
2019-03-16 16:18:16 -0300

    Estrutura inical do projeto

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={App} />
+            {/* <Route path="/localizacao" component={Location} /> */}
+            {/* <Route path='*' component={Error404} /> */}
+        </Switch>
+    </ BrowserRouter>
+    , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
