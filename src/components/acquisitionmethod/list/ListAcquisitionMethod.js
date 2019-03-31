2de4cf969e0c099d85290141d9c058e8c0836d63
Guilherme Aguiar <gui-aguiar9@hotmail.com>
2019-03-31 16:53:46 -0300

    Consulta Método de Aquisição

--- src/components/acquisitionmethod/list/ListAcquisitionMethod.js
+++ src/components/acquisitionmethod/list/ListAcquisitionMethod.js
@@ -1,14 +1,68 @@
 import React, {Component} from 'react';
+import {InputText} from 'primereact/inputtext';
+import {Dropdown} from 'primereact/dropdown';
+import {Button} from 'primereact/button';
+import {DataTable} from 'primereact/datatable';
+import {Column} from 'primereact/column';
+import {AcquisitionService} from '../../../service/AcquisitionService';
 
 export class ListAcquisitionMethod extends Component {
+    constructor() {
+        super();
+        this.state = {
+            code: null,
+            description: null,
+            acquisitionState: null,
+            acquisitions: []
+        };
+        this.acquisitionservice = new AcquisitionService();
+        this.onacquisitionStateChange = this.onacquisitionStateChange.bind(this);
+    }
 
+    onacquisitionStateChange(e) {
+        this.setState({acquisitionState: e.value});
+    }
+
+    componentDidMount() {
+        this.acquisitionservice.getAcquisitionsSmall().then(data => this.setState({acquisitions: data}));
+    }
     render() {
+        const acquistionStates = [
+            {name: 'Ativo', value: 'Ativo'},
+            {name: 'Inativo', value: 'Inativo'}
+        ];
+
         return (
-            <div className="p-grid">
+            <div className="p-col-12">
+                    <div className="card">
+                        <h1>Consulta de Método de Aquisição</h1>
+                        <div className="search-box">
+
+                            <div className="search-box-child">
+                                <h3 className="first"  style={{marginRight:'10px'}}>Código</h3>
+                                <InputText type="text" keyfilter="pint" code={this.state.code} onChange={(e) => this.setState({code: e.target.codigo})} style={{width:'75px'}} />
+                            </div>
+
+                            <div className="search-box-child">
+                                <h3 className="first" style={{marginRight:'10px'}}>Descrição</h3>
+                                <InputText type="text" size="20" description={this.state.description} onChange={(e) => this.setState({description: e.target.description})} />
+                            </div>
+
+                            <div className="search-box-child">
+                                <h3 className="first" style={{marginRight:'10px'}}>Estado</h3>
+                                <Dropdown value={this.state.aquisitionState} options={acquistionStates} onChange={this.onacquisitionStateChange} style={{width:'180px'}} placeholder="Selecione um Estado" optionLabel="name"/>
+                            </div>
+
+                            <br></br><Button type="submit" id="btnSearch" label="Consultar" className="p-button-primary" icon="pi pi-search" />
+                        </div>
+                    </div>
                 <div className="p-col-12">
                     <div className="card">
-                        <h1>Empty Page</h1>
-                        <p>Use this page to start from scratch and place your custom content.</p>
+                        <DataTable value={this.state.acquisitions} paginator={true} rows={10} rowsPerPageOptions={[10, 30, 50, 100]}>
+                            <Column field="code" header="Código" sortable={true}/>
+                            <Column field="method" header="Método de Aquisição" sortable={true}/>
+                            <Column field="state" header="Estado" sortable={true}/>
+                        </DataTable>
                     </div>
                 </div>
             </div>
---
+++ src/service/AcquisitionService.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export class AcquisitionService {
+    
+    getAcquisitionsSmall() {
+        return axios.get('assets/demo/data/cars-small.json')
+                .then(res => res.data.data);
+    }
+
+    getAcquisitionsMedium() {
+        return axios.get('assets/demo/data/cars-medium.json')
+                .then(res => res.data.data);
+    }
+
+    getAcquisitionsLarge() {
+        return axios.get('assets/demo/data/cars-large.json')
+                .then(res => res.data.data);
+    }
+}
